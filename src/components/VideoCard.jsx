8c086429f03fcef2b4995bc48f3129ae336fc3ed
NareshBhattarai4 <technologyandapplication@gmail.com>
2024-03-21 17:13:31 +0545

    new changes

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -4,7 +4,7 @@ import thumbnail from "../assets/thumbnail.jpeg"
 
 const VideoCard = () => {
   return (
- <div className="w-[100vw] h-[100%] z-[-4] bg-gray-800  grid grid-cols-1 gap-4 px-2 mr-18 text-white md:mr-14 sm:grid-cols-2 md:grid-cols-4 md:w-[82.5vw] " >
+ <div className="w-[100vw] h-[100%] z-[-4] bg-gray-800  grid grid-cols-1 gap-4 px-2 mr-24 text-white md:mr-14 sm:grid-cols-2 md:grid-cols-4 md:w-[82.5vw] " >
 
   <div className="flex flex-col items-center gap-2 w-[100%]  cursor-pointer ">
     <div className="relative">
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ const Home = ({modal}) => {
   return (
     <div className='bg-gray-800 w-[100vw]' >
        <Sidebar open={modal}/>
-        <div className="pl-[0.4em] mt-4 md:pl-[16.5em]">
+        <div className="pl-[0.0em] mt-4 md:pl-[16.5em]">
         <VideoCard/>
         </div>
        
