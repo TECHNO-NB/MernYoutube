e9f3b35da6f10003acf4358fbdeb1efe3ddb279c
NareshBhattarai4 <technologyandapplication@gmail.com>
2024-03-21 16:35:51 +0545

    New Changes

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,71 +1,82 @@
-import React, { useState } from 'react'
-import { AiOutlineHome,AiOutlineLike } from "react-icons/ai";
+import React, { useState } from "react";
+import { AiOutlineHome, AiOutlineLike } from "react-icons/ai";
 import { LuTimerReset } from "react-icons/lu";
 import { BsCameraVideo } from "react-icons/bs";
 import { GoFileDirectory } from "react-icons/go";
 import { RiUserFollowLine } from "react-icons/ri";
 import { IoSettingsOutline } from "react-icons/io5";
 
-
-const Sidebar = ({open}) => {
- return (
+const Sidebar = ({ open }) => {
+  return (
     <>
-    <div className={`fixed ${open.modal ? "left-0" : "left-[-16.5em]"} bg-none transition-all z-20 bg-gray-800 w-[55vw] h-[119vh] border-r-2 border-white md:left-[0em] md:w-[17vw] md:h-[100vh]`}>
-    <div className=" flex flex-col items-center text-center gap-[7px] border-x-5 border-red-400 ">
-        <div className="flex items-center py-[5px] mt-5 rounded-sm text-white border-2 w-[95%]  gap-4 text-[1.2em] cursor-pointer hover:bg-purple-800 ">
-        <AiOutlineHome className='ml-2'/>
-        <span>Home</span>
-        </div>
-        <div className=" flex items-center py-[5px] bg-none rounded-sm text-white border-2 w-[95%]  gap-4 text-[1.2em] cursor-pointer hover:bg-purple-800 ">
-        <AiOutlineLike className='ml-2'/>
-        <span>Liked Videos</span>
-        </div>
-        <div className=" flex items-center py-[5px]  rounded-sm text-white border-2 w-[95%]  gap-4 text-[1.2em] cursor-pointer hover:bg-purple-800 ">
-        <LuTimerReset className='ml-2'/>
-        <span>History</span>
+      <div
+        className={`fixed ${
+          open.modal ? "left-0" : "left-[-16.5em]"
+        } bg-none transition-all z-20 bg-gray-800 w-[55vw]  h-[119vh] border-r-2 border-white md:left-[0em] md:w-[17vw] md:h-[100vh]`}
+      >
+        <div className=" flex flex-col items-center text-center gap-[7px] border-x-5 border-red-400 ">
+          <div className="flex items-center py-[5px] mt-5 rounded-sm text-white border-2 w-[95%]  gap-4 text-[1.2em] cursor-pointer hover:bg-purple-800 ">
+            <AiOutlineHome className="ml-2" />
+            <span>Home</span>
+          </div>
+          <div className=" flex items-center py-[5px] bg-none rounded-sm text-white border-2 w-[95%]  gap-4 text-[1.2em] cursor-pointer hover:bg-purple-800 ">
+            <AiOutlineLike className="ml-2" />
+            <span>Liked Videos</span>
+          </div>
+          <div className=" flex items-center py-[5px]  rounded-sm text-white border-2 w-[95%]  gap-4 text-[1.2em] cursor-pointer hover:bg-purple-800 ">
+            <LuTimerReset className="ml-2" />
+            <span>History</span>
+          </div>
+
+          <div className=" flex items-center py-[5px]  rounded-sm text-white border-2 w-[95%]  gap-4 text-[1.2em] cursor-pointer hover:bg-purple-800 ">
+            <BsCameraVideo className="ml-2" />
+            <span>My Content</span>
+          </div>
+
+          <div className=" flex items-center py-[5px]  rounded-sm text-white border-2 w-[95%]  gap-4 text-[1.2em] cursor-pointer hover:bg-purple-800 ">
+            <GoFileDirectory className="ml-2" />
+            <span>Collections</span>
+          </div>
+          <div className=" flex items-center py-[5px]  rounded-sm text-white border-2 w-[95%]  gap-4 text-[1.2em] cursor-pointer hover:bg-purple-800 ">
+            <RiUserFollowLine className="ml-2" />
+            <span>Subscribers</span>
+          </div>
         </div>
 
-        <div className=" flex items-center py-[5px]  rounded-sm text-white border-2 w-[95%]  gap-4 text-[1.2em] cursor-pointer hover:bg-purple-800 ">
-        <BsCameraVideo className='ml-2'/>
-        <span>My Content</span>
-        </div>
 
-        <div className=" flex items-center py-[5px]  rounded-sm text-white border-2 w-[95%]  gap-4 text-[1.2em] cursor-pointer hover:bg-purple-800 ">
-        <GoFileDirectory className='ml-2'/>
-        <span>Collections</span>
-        </div>
-        <div className=" flex items-center py-[5px]  rounded-sm text-white border-2 w-[95%]  gap-4 text-[1.2em] cursor-pointer hover:bg-purple-800 ">
-        <RiUserFollowLine className='ml-2'/>
-        <span>Subscribers</span>
-        </div>
+ {/* sideBar Bottom Part */}
+       <div className="flex flex-col mt-[184%] items-center gap-2 md:mt-[105%]">
+          {/* For Pic */}
+
+          <div className="hidden  md:flex md:items-center md:w-full md:flex-col md:mt-[20%] ">
+<div className=" flex items-center py-[5px]  justify-center  rounded-sm text-white border-2 w-[95%]  gap-4 text-[1.2em] cursor-pointer hover:bg-purple-800">
+         <span >Settings</span>
+        </div> 
  
-    </div>
-    <div className=" bottom-0 absolute mb-2 flex  flex-col items-center text-center gap-[7px] border-x-5 border-red-400 w-[100%] md:bottom-0 md:opacity-100 ">
-    <div className=" flex items-center py-[5px] opacity-0 rounded-sm text-white border-2 w-[95%]  gap-4 text-[1.2em] cursor-pointer hover:bg-purple-800 md:opacity-100 md:mt-150">
-        <IoSettingsOutline className='ml-2  '/>
-        <span >Settings</span>
         </div>
 
-        <div className=" flex items-center py-[5px] opacity-100 justify-center  rounded-sm text-white border-2 w-[95%]  gap-4 text-[1.2em] cursor-pointer hover:bg-purple-800 md:opacity-0">
+          {/* pc end */}
         
-        <span >Log in</span>
+    {/* For mobile */}
+      <div className="w-full flex flex-col gap-2 items-center md:hidden  ">
+<div className=" flex items-center py-[5px]  justify-center  rounded-sm text-white border-2 w-[95%]  gap-4 text-[1.2em] cursor-pointer hover:bg-purple-800">
+         <span >Log in</span>
+        </div> 
+ <div className="flex items-center py-[5px] text-center justify-center  bg-[#AE7AFF]  rounded-sm text-white border-2 w-[95%]  gap-4 text-[1.2em] cursor-pointer hover:bg-purple-800  ">
+       <span >Sign up</span>
+        </div>
         </div>
 
-        <div className="flex items-center py-[5px] text-center justify-center  bg-[#AE7AFF] opacity-100 rounded-sm text-white border-2 w-[95%]  gap-4 text-[1.2em] cursor-pointer hover:bg-purple-800 md:opacity-0 ">
-   
-        <span >Sign up</span>
-        </div>
-
-
-
+{/* mobile end */}
+</div>
+</div>
         
-    </div>
 
-    
-   
-    </div>
+
+
+     
     </>
-  ) 
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,198 +1,34 @@
 import React from 'react'
 import thumbnail from "../assets/thumbnail.jpeg"
-import thumbnail2 from "../assets/photo.webp";
-import logo3 from "../assets/logo3.webp";
-import logo4 from "../assets/logo4.jpeg";
-import logo5 from "../assets/logo5.webp";
-import img from "../assets/img.webp";
-import img2 from "../assets/img2.jpeg";
-import img3 from "../assets/img3.jpeg";
-import img5 from "../assets/img5.jpeg";
+
 
 const VideoCard = () => {
   return (
  <div className="w-[100vw] h-[100%] z-[-4] bg-gray-800  grid grid-cols-1 gap-4 px-2 mr-14 text-white sm:grid-cols-2 md:grid-cols-4 md:w-[82.5vw] " >
 
-<div className="flex flex-col items-center gap-2 w-[100%]  cursor-pointer ">
+  <div className="flex flex-col items-center gap-2 w-[100%]  cursor-pointer ">
     <div className="relative">
     <img  className="w-[100%]"src={thumbnail}></img>
     <p className='absolute bottom-2 right-2 bg-black px-2'>20:45</p>
     </div>
-    <div className="">
-      <div className="flex items-center gap-2">
+    <div className="w-full">
+      <div className="flex gap-2">
         <img className='w-10 h-10 rounded-[50%]'  src='./src/assets/logo2.jpeg'/>
-        <h1 className='text-[1em]'>JavaScript Fundamentals: Variables and Data Types</h1>
+
+
+        <div className="div">
+            <h1 className='text-white text-[1.2em]'>JavaScript Fundamentals:</h1>
+        <p className=' text-gray-200 font-[300]'>10.3k Views · <span>44 minutes ago</span></p>
+        <p className=' text-gray-200 font-[400]'>Code Master</p>
+        </div>
+      
       </div>
-      <p className='ml-12 text-gray-200 font-[300]'>10.3k Views · <span>44 minutes ago</span></p>
-      <p className='ml-12 text-gray-200 font-[400]'>Code Master</p>
-    </div>
-    </div>
-
-
-    <div className="flex flex-col items-center gap-2 w-[100%]  cursor-pointer">
-    <div className="relative">
-    <img  className="w-[100%]"src={thumbnail2}></img>
-    <p className='absolute bottom-2 right-2 bg-black px-2'>20:45</p>
-    </div>
-    <div className="">
-      <div className="flex items-center gap-2">
-        <img className='w-10 h-10 rounded-[50%]'  src={logo3}/>
-        <h1 className='text-[1em]'>
-Getting Started with Express.js</h1>
-      </div>
-      <p className='ml-12 text-gray-200 font-[300]'>11.k Views · <span>5 hours ago</span></p>
-      <p className='ml-12 text-gray-200 font-[400]'>Express Learner</p>
-    </div>
-    </div>
-
-
-
-    <div className="flex flex-col items-center gap-2 w-[100%]  cursor-pointer">
-    <div className="relative">
-    <img  className="w-[100%]"src={img}></img>
-    <p className='absolute bottom-2 right-2 bg-black px-2'>20:45</p>
-    </div>
-    <div className="">
-      <div className="flex items-center gap-2">
-        <img className='w-10 h-10 rounded-[50%]'  src={img2}/>
-        <h1 className='text-[1em]'>Node.js Authentication with Passport.js</h1>
-      </div>
-      <p className='ml-12 text-gray-200 font-[300]'>100.3k Views · <span>44 minutes ago</span></p>
-      <p className='ml-12 text-gray-200 font-[400]'>Passport Pro</p>
-    </div>
-    </div>
-
-
-
-
-    <div className="flex flex-col items-center gap-2 w-[100%] cursor-pointer ">
-    <div className="relative">
-    <img  className="w-[100%]"src={img3} ></img>
-    <p className='absolute bottom-2 right-2 bg-black px-2'>20:45</p>
-    </div>
-    <div className="">
-      <div className="flex items-center gap-2">
-        <img className='w-10 h-10 rounded-[50%]'  src={logo5}/>
-        <h1 className='text-[1em]'>Building a RESTful API with Node.js and Express</h1>
-      </div>
-      <p className='ml-12 text-gray-200 font-[300]'>10.3k Views · <span>44 minutes ago</span></p>
-      <p className='ml-12 text-gray-200 font-[400]'>React Patterns</p>
-    </div>
-    </div>
-
-
-
-    <div className="flex flex-col items-center gap-2 w-[100%] cursor-pointer ">
-    <div className="relative">
-    <img  className="w-[100%]"src={img5} ></img>
-    <p className='absolute bottom-2 right-2 bg-black px-2'>20:45</p>
-    </div>
-    <div className="">
-      <div className="flex items-center gap-2">
-        <img className='w-10 h-10 rounded-[50%]'  src={logo5}/>
-        <h1 className='text-[1em]'>Building a RESTful API with Node.js and Express</h1>
-      </div>
-      <p className='ml-12 text-gray-200 font-[300]'>10.3k Views · <span>44 minutes ago</span></p>
-      <p className='ml-12 text-gray-200 font-[400]'>React Patterns</p>
-    </div>
-    </div>
-
-
-
-    <div className="flex flex-col items-center gap-2 w-[100%]  cursor-pointer ">
-    <div className="relative">
-    <img  className="w-[100%]"src={thumbnail}></img>
-    <p className='absolute bottom-2 right-2 bg-black px-2'>20:45</p>
-    </div>
-    <div className="">
-      <div className="flex items-center gap-2">
-        <img className='w-10 h-10 rounded-[50%]'  src='./src/assets/logo2.jpeg'/>
-        <h1 className='text-[1em]'>JavaScript Fundamentals: Variables and Data Types</h1>
-      </div>
-      <p className='ml-12 text-gray-200 font-[300]'>10.3k Views · <span>44 minutes ago</span></p>
-      <p className='ml-12 text-gray-200 font-[400]'>Code Master</p>
-    </div>
-    </div>
-
-
-    <div className="flex flex-col items-center gap-2 w-[100%]  cursor-pointer">
-    <div className="relative">
-    <img  className="w-[100%]"src={thumbnail2}></img>
-    <p className='absolute bottom-2 right-2 bg-black px-2'>20:45</p>
-    </div>
-    <div className="">
-      <div className="flex items-center gap-2">
-        <img className='w-10 h-10 rounded-[50%]'  src={logo3}/>
-        <h1 className='text-[1em]'>
-Getting Started with Express.js</h1>
-      </div>
-      <p className='ml-12 text-gray-200 font-[300]'>11.k Views · <span>5 hours ago</span></p>
-      <p className='ml-12 text-gray-200 font-[400]'>Express Learner</p>
-    </div>
-    </div>
-
-
-
-    <div className="flex flex-col items-center gap-2 w-[100%]  cursor-pointer">
-    <div className="relative">
-    <img  className="w-[100%]"src={img}></img>
-    <p className='absolute bottom-2 right-2 bg-black px-2'>20:45</p>
-    </div>
-    <div className="">
-      <div className="flex items-center gap-2">
-        <img className='w-10 h-10 rounded-[50%]'  src={img2}/>
-        <h1 className='text-[1em]'>Node.js Authentication with Passport.js</h1>
-      </div>
-      <p className='ml-12 text-gray-200 font-[300]'>100.3k Views · <span>44 minutes ago</span></p>
-      <p className='ml-12 text-gray-200 font-[400]'>Passport Pro</p>
-    </div>
-    </div>
-
-
-
-
-    <div className="flex flex-col items-center gap-2 w-[100%] cursor-pointer ">
-    <div className="relative">
-    <img  className="w-[100%]"src={img3} ></img>
-    <p className='absolute bottom-2 right-2 bg-black px-2'>20:45</p>
-    </div>
-    <div className="">
-      <div className="flex items-center gap-2">
-        <img className='w-10 h-10 rounded-[50%]'  src={logo5}/>
-        <h1 className='text-[1em]'>Building a RESTful API with Node.js and Express</h1>
-      </div>
-      <p className='ml-12 text-gray-200 font-[300]'>10.3k Views · <span>44 minutes ago</span></p>
-      <p className='ml-12 text-gray-200 font-[400]'>React Patterns</p>
-    </div>
-    </div>
-
-
-
-    <div className="flex flex-col items-center gap-2 w-[100%] cursor-pointer ">
-    <div className="relative">
-    <img  className="w-[100%]"src={img5} ></img>
-    <p className='absolute bottom-2 right-2 bg-black px-2'>20:45</p>
-    </div>
-    <div className="">
-      <div className="flex items-center gap-2">
-        <img className='w-10 h-10 rounded-[50%]'  src={logo5}/>
-        <h1 className='text-[1em]'>Building a RESTful API with Node.js and Express</h1>
-      </div>
-      <p className='ml-12 text-gray-200 font-[300]'>10.3k Views · <span>44 minutes ago</span></p>
-      <p className='ml-12 text-gray-200 font-[400]'>React Patterns</p>
-    </div>
-    </div>
-
-
-
     
-
-
-
-
-
-
     </div>
+    </div>
+
+
+   </div>
  
 
   )
