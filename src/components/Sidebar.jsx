b54750fcdfbf3b899c47895e47230d83eee8c2b5
NareshBhattarai4 <technologyandapplication@gmail.com>
2024-03-22 15:11:39 +0545

    new

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -57,7 +57,7 @@ const Sidebar = () => {
         </div>
 
         {/* sideBar Bottom Part */}
-        <div className="flex flex-col mt-[142.5%] items-center gap-2 md:mt-[105%]">
+        <div className="flex flex-col mt-[140%] items-center gap-2 md:mt-[105%]">
           {/* For Pic */}
           <div className="hidden  md:flex md:items-center md:w-full md:flex-col md:mt-[20%] ">
             <div className=" flex items-center py-[5px]  justify-center  rounded-sm text-white border-2 w-[95%]  gap-4 text-[1.2em] cursor-pointer hover:bg-purple-800">
