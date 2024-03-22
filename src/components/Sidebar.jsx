55959b2eedaa608da78b3c8f1d29fa385b243c60
NareshBhattarai4 <technologyandapplication@gmail.com>
2024-03-22 15:09:46 +0545

    new chnages

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React, { useState,useContext} from "react";
+import React, { useState, useContext } from "react";
 import { AiOutlineHome, AiOutlineLike } from "react-icons/ai";
 import { LuTimerReset } from "react-icons/lu";
 import { BsCameraVideo } from "react-icons/bs";
@@ -6,23 +6,29 @@ import { GoFileDirectory } from "react-icons/go";
 import { RiUserFollowLine } from "react-icons/ri";
 import { IoSettingsOutline } from "react-icons/io5";
 import { useNavigate } from "react-router-dom";
-import {showhidesidebar} from "../context/GlobalContext"
-
+import { showhidesidebar } from "../context/GlobalContext";
 
 const Sidebar = () => {
-  const {open,setOpen}=useContext(showhidesidebar);
-  const navigate=useNavigate();
-  const handleNavigate=(path)=>{
-    navigate(path)
-    setOpen(true)
-  }
+  const { open, setOpen } = useContext(showhidesidebar);
+  const navigate = useNavigate();
+  const handleNavigate = (path) => {
+    navigate(path);
+    setOpen(true);
+  };
   return (
     <>
       <div
-        className={`fixed ${open ? "left-[-14em]" : "left-[0em]" } bg-none transition-all z-20 bg-gray-800 w-[55vw]  h-[100vh] border-r-2 border-white md:left-[0em] md:w-[17vw] md:h-[100vh]`}
+        className={`fixed ${
+          open ? "left-[-14em]" : "left-[0em]"
+        } bg-none transition-all z-20 bg-gray-800 w-[55vw]  h-[100vh] border-r-2 border-white md:left-[0em] md:w-[17vw] md:h-[100vh]`}
       >
         <div className=" flex flex-col items-center text-center gap-[7px] border-x-5 border-red-400 ">
-          <div  onClick={()=>{handleNavigate("/")}} className="flex items-center py-[5px] mt-0 rounded-sm text-white border-2 w-[95%]  gap-4 text-[1.2em] cursor-pointer hover:bg-purple-800 ">
+          <div
+            onClick={() => {
+              handleNavigate("/");
+            }}
+            className="flex items-center py-[5px] mt-0 rounded-sm text-white border-2 w-[95%]  gap-4 text-[1.2em] cursor-pointer hover:bg-purple-800 "
+          >
             <AiOutlineHome className="ml-2" />
             <span>Home</span>
           </div>
@@ -50,38 +56,30 @@ const Sidebar = () => {
           </div>
         </div>
 
-
- {/* sideBar Bottom Part */}
-       <div className="flex flex-col mt-[145%] items-center gap-2 md:mt-[105%]">
+        {/* sideBar Bottom Part */}
+        <div className="flex flex-col mt-[142.5%] items-center gap-2 md:mt-[105%]">
           {/* For Pic */}
-
           <div className="hidden  md:flex md:items-center md:w-full md:flex-col md:mt-[20%] ">
-<div className=" flex items-center py-[5px]  justify-center  rounded-sm text-white border-2 w-[95%]  gap-4 text-[1.2em] cursor-pointer hover:bg-purple-800">
-         <span >Settings</span>
-        </div> 
- 
-        </div>
+            <div className=" flex items-center py-[5px]  justify-center  rounded-sm text-white border-2 w-[95%]  gap-4 text-[1.2em] cursor-pointer hover:bg-purple-800">
+              <span>Settings</span>
+            </div>
+          </div>
 
           {/* pc end */}
-        
-    {/* For mobile */}
-      <div className="w-full flex flex-col gap-2 items-center md:hidden  ">
-<div className=" flex items-center py-[5px]  justify-center  rounded-sm text-white border-2 w-[95%]  gap-4 text-[1.2em] cursor-pointer hover:bg-purple-800">
-         <span >Log in</span>
-        </div> 
- <div className="flex items-center py-[5px] text-center justify-center  bg-[#AE7AFF]  rounded-sm text-white border-2 w-[95%]  gap-4 text-[1.2em] cursor-pointer hover:bg-purple-800  ">
-       <span >Sign up</span>
+
+          {/* For mobile */}
+          <div className="w-full flex flex-col gap-2 items-center md:hidden  ">
+            <div className=" flex items-center py-[5px]  justify-center  rounded-sm text-white border-2 w-[95%]  gap-4 text-[1.2em] cursor-pointer hover:bg-purple-800">
+              <span>Log in</span>
+            </div>
+            <div className="flex items-center py-[5px] text-center justify-center  bg-[#AE7AFF]  rounded-sm text-white border-2 w-[95%]  gap-4 text-[1.2em] cursor-pointer hover:bg-purple-800  ">
+              <span>Sign up</span>
+            </div>
+          </div>
+
+          {/* mobile end */}
         </div>
-        </div>
-
-{/* mobile end */}
-</div>
-</div>
-        
-
-
-
-     
+      </div>
     </>
   );
 };
--- src/videoplay/Comments.jsx
+++ src/videoplay/Comments.jsx
@@ -5,7 +5,7 @@ const Comments = () => {
     <div className="w-full items-center mt-2 px-2 justify-center border-2 pb-2 text-white ">
       <h4>All Comments</h4>
       <input
-        className="w-full px-2 mt-1 bg-gray-800 outline-white border-2 border-white placeholder-white"
+        className="w-full h-8 px-2 mt-1 bg-gray-800 outline-white border-2 border-white placeholder-white"
         type="text"
         placeholder="Add comments.."
       />
